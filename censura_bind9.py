17cdd22c96b280166c4dced41912b6bb06cb8ad9
Joandson Bezerra <36518985+joandson19@users.noreply.github.com>
2024-04-23 16:42:35 -0300

    Add files via upload

---
+++ censura_bind9.py
@@ -0,0 +1,48 @@
+import os
+import datetime
+import sys
+import requests
+
+def get_serial_number():
+
+    today = datetime.date.today()
+    return today.strftime("%Y%m%d01")
+
+def create_rpz_zone_file(domain_list, output_file, var_domain):
+
+    serial_number = get_serial_number()
+    with open(output_file, 'w') as output:
+        output.write(f"$TTL 1H\n@       IN      SOA LOCALHOST. {var_domain}. (\n")
+        output.write(f"                {serial_number}      ; Serial\n")
+        output.write("                1h              ; Refresh\n")
+        output.write("                15m             ; Retry\n")
+        output.write("                30d             ; Expire\n")
+        output.write("                2h              ; Negative Cache TTL\n        )\n")
+        output.write(f"        NS  {var_domain}.\n\n")
+
+        for domain in domain_list:
+            domain = domain.strip()
+            output.write(f"{domain} IN CNAME .\n")
+            output.write(f"*.{domain} IN CNAME .\n")
+
+def main(var_domain):
+
+    url = 'https://alagoinhastelecom.com.br/censura/exportar_dominios.php'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        domain_list = response.text.split('\n')
+        domain_list = [domain.strip() for domain in domain_list if domain.strip()]
+
+        rpz_zone_file = '/var/cache/bind/rpz/db.rpz.zone.hosts'
+
+        create_rpz_zone_file(domain_list, rpz_zone_file, var_domain)
+        print("Arquivo de zona RPZ atualizado.")
+    else:
+        print("Falha ao obter os domínios.")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python3 script.py sub.dominio.com.br")
+        sys.exit(1)
+    main(sys.argv[1])
---
+++ censura_unbound.py
@@ -0,0 +1,39 @@
+import datetime
+import sys
+import requests
+
+def get_serial_number():
+
+    today = datetime.date.today()
+    return today.strftime("%Y%m%d01")
+
+def create_unbound_config(domain_list, output_file):
+
+    serial_number = get_serial_number()
+    with open(output_file, 'w') as output:
+        output.write(f'# Arquivo de configuração gerado automaticamente em {datetime.datetime.now()}\n\n')
+
+        for domain in domain_list:
+            domain = domain.strip()
+            output.write(f'local-zone: "{domain}" always_nxdomain\n')
+
+        output.write('\n# Fim do arquivo de configuração\n')
+
+def main():
+
+    url = 'https://alagoinhastelecom.com.br/censura/exportar_dominios.php'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        domain_list = response.text.split('\n')
+        domain_list = [domain.strip() for domain in domain_list if domain.strip()]
+
+        unbound_config_file = '/tmp/local-block.conf'
+
+        create_unbound_config(domain_list, unbound_config_file)
+        print("Arquivo de configuração para Unbound atualizado.")
+    else:
+        print("Falha ao obter os domínios.")
+
+if __name__ == "__main__":
+    main()
